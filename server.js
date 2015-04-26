cfa2b41be296a7460e935504fd8a45fe423f3047
philipz <philipzheng@gmail.com>
2015-04-26 11:13:15 +0800

    add CI

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', function(req, res){
   var message = process.env.POWERED_BY;
   if (typeof(message) == "undefined") {
-  	message = "Deis & Philipz"
+  	message = "Deis & Philipz & Travis-CI"
   }
   res.send('Powered by ' + message);
 });
